ed06742e5dee2db8135ab3a5658590016a5b3a01
hotbari <138793425+hotbari@users.noreply.github.com>
2024-10-27 14:32:23 +0900

    add load_dotenv()

--- config/settings/base.py
+++ config/settings/base.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
-
 from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,6 +76,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
 
 DATABASES = {
     "default": {
