5b10d9dc799972820fe42076effb76873f4379dd
hotbari <138793425+hotbari@users.noreply.github.com>
2024-11-05 00:11:05 +0900

    no user, database

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,14 @@ from .base import *
 ALLOWED_HOSTS = ['52.78.145.93', '127.0.0.1']
 
 DEBUG = False  # 배포 환경에서는 False
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    }
+}
--- users/models.py
+++ users/models.py
@@ -1,11 +1,6 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-# Create your models here.
-class User(AbstractUser):
-    email = models.EmailField(unique=True)
-    USERNAME_FIELD = 'email'
-    username = models.CharField(max_length=50, unique=True)
-    nickname = models.CharField(max_length=120)
-    password = models.CharField(max_length=120)
-    phone_number = models.CharField(max_length=120)
+# from django.contrib.auth.models import AbstractUser, Group, Permission
+# from django.db import models
+#
+#
+# class User(AbstractUser):
+#     pass
