bf5487ad613f96834388fdeaff2b1d789bc6db7a
hotbari <ghkahr01@naver.com>
2024-11-26 14:15:53 +0900

    feat: Add Accounts Model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from users.models import User
+
+class Account(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    account_number = models.CharField(max_length=30)
+    bank_code = models.CharField(max_length=10)
+    account_type = models.CharField(max_length=20)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f"{self.account_number} ({self.bank_code})"
 
 # Create your models here.
--- users/models.py
+++ users/models.py
@@ -1,6 +1,4 @@
-# from django.contrib.auth.models import AbstractUser, Group, Permission
-# from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
 #
-#
-# class User(AbstractUser):
-#     pass
+class User(AbstractUser):
+    pass
